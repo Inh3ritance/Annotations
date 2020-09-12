f82a3bf3e303c65d6ff159f34b77f6ae61aac777
Inh3ritance <herreralaces@gmail.com>
2020-09-11 18:37:18 -0700

    Added Bezier

--- annotations/src/App.js
+++ annotations/src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import Bezier from './Bezier.js'
 import './App.css';
-import { MarkerArea } from 'markerjs';
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" id='myImage'/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bezier></Bezier>
     </div>
   );
 }
